6133f02dc43b56e31b6bb8c1c5416737dac41b97
bkrztuk <bkrztuk@gavick.com>
2013-06-19 08:43:50 +0200

    Display errors fix

--- meet_gavern/index.php
+++ meet_gavern/index.php
@@ -15,8 +15,8 @@ defined('_JEXEC') or die;
 // to avoid the problems
 define('DS', DIRECTORY_SEPARATOR);
 // enable showing errors in PHP
-ini_set('error_reporting', E_ALL);
-ini_set('display_errors','On');
+//ini_set('error_reporting', E_ALL);
+//ini_set('display_errors','On');
 // include framework classes and files
 require_once('lib/gk.framework.php');
 // run the framework
