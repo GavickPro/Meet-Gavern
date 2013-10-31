546cf7053c8088c11f902919753e86ab40a5a1e9
bkrztuk <bkrztuk@gavick.com>
2013-10-31 22:06:41 +0100

    Missing URL quotes
    - closes #142
    - closes #90
    - closes #98

--- meet_gavern/admin/elements/url.php
+++ meet_gavern/admin/elements/url.php
@@ -12,9 +12,9 @@ class JFormFieldUrl extends JFormField {
 		$url	= (string) $this->element['url'];
 		$text  	= (string) $this->element['text'];
 		
-		return '<div class="control-label gkFont"><a class="gkFormLink" href="http://www.google.com/fonts/" target="_blank">' . JText::_(TPL_GK_LANG_GOOGLE_FONTS_URL) . '</a></div>'.
-				'<div class="control-label gkFont"><a class="gkFormLink" href="http://www.fontsquirrel.com/fontface" target="_blank">' . JText::_(TPL_GK_LANG_SQUIRREL_FONTS_URL) . '</a></div>'.
-				'<div class="control-label gkFont"><a class="gkFormLink" href="http://html.adobe.com/edge/webfonts/" target="_blank">' . JText::_(TPL_GK_LANG_EDGE_FONTS_URL) . '</a></div>';
+		return '<div class="control-label gkFont"><a class="gkFormLink" href="http://www.google.com/fonts/" target="_blank">' . JText::_('TPL_GK_LANG_GOOGLE_FONTS_URL') . '</a></div>'.
+				'<div class="control-label gkFont"><a class="gkFormLink" href="http://www.fontsquirrel.com/fontface" target="_blank">' . JText::_('TPL_GK_LANG_SQUIRREL_FONTS_URL') . '</a></div>'.
+				'<div class="control-label gkFont"><a class="gkFormLink" href="http://html.adobe.com/edge/webfonts/" target="_blank">' . JText::_('TPL_GK_LANG_EDGE_FONTS_URL') . '</a></div>';
 	}
 }
 
