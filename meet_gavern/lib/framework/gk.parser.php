d1fb91cd8777afc9aa0fb87f685e3080240b9495
dziudek <dziudek@gmail.com>
2013-11-05 09:34:27 +0100

    Improvements in the GKParser class
    * better comments
    * removed unnecessary bInfo variable
    * renamed method parseIt to parse
    * changed the method parse visibility to private

--- meet_gavern/lib/framework/gk.parser.php
+++ meet_gavern/lib/framework/gk.parser.php
@@ -4,9 +4,9 @@
  *
  * GKParser class
  *
- * @version             3.0.0
+ * @version             4.0.0
  * @package             Gavern Framework
- * @copyright			Copyright (C) 2010 - 2012 GavickPro. All rights reserved.
+ * @copyright			Copyright (C) 2010 - 2013 GavickPro. All rights reserved.
  *               
  */
  
@@ -24,29 +24,30 @@ defined('_JEXEC') or die;
 
 class GKParser {	
     static public $customRules = array();
-    
+    // access to the Joomla! output
     public $body;
-    private $bInfo;
-
-    public function __construct()
-    {
+	// initialize the GKParser
+    public function __construct() {
+    	// import the JResponse class
         jimport('joomla.environment.response');
+        // get the final output o the Joomla Website
         $this->body = JResponse::getBody();
-        $this->bInfo = null;
-        $buf = $this->parseIt();
+        // make all stored replacements
+        $buf = $this->parse();
+        // set the modified output as a final output of website
         JResponse::setBody($buf);
     }
-
-    public function parseIt()
-    {
+	// method used to parse the content
+    private function parse() {
     	// if the custom rules are defined
     	if(count(self::$customRules)) {
     		// use it for parsing the website
     		foreach (self::$customRules as $pattern => $replace) {
+    		    // replace in the body all patterns
     		    $this->body = preg_replace($pattern, $replace, $this->body);
     		}
     	} 
-        
+        // return the body with replaced elements
         return $this->body;
     }
 }
