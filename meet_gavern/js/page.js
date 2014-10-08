f000b893a703babdf447918fcfd2e6ab67c464d8
Tomasz Dziuda <dziudek@gmail.com>
2014-10-08 10:07:57 +0200

    Fixed issues with the aside overlapping

--- meet_gavern/js/page.js
+++ meet_gavern/js/page.js
@@ -26,9 +26,52 @@ jQuery(window).load(function() {
 		    }
 		});
 	});
-	
-
 });
 
 var JCaption = function() {};
 var Tips = function() {};
+
+// Fix for the columns overlap
+jQuery(document).ready(function() {
+	if(jQuery('.itemBody').length > 0) {
+		gk_columns_resize(jQuery('.itemBody'));
+		jQuery(window).resize(function() {
+			gk_columns_resize(jQuery('.itemBody'));
+		});
+	}
+	
+	if(jQuery('.gk-article').length > 0) {
+		gk_columns_resize(jQuery('.gk-article'));
+		jQuery(window).resize(function() {
+			gk_columns_resize(jQuery('.gk-article'));
+		});
+	}
+});
+
+jQuery(window).load(function() {
+	if(jQuery('.itemBody').length > 0) {
+		gk_columns_resize(jQuery('.itemBody'));
+	}
+	
+	if(jQuery('.gk-article').length > 0) {
+		gk_columns_resize(jQuery('.gk-article'));
+	}
+});
+
+function gk_columns_resize(items) {
+	if(jQuery(window).width() > 480) {
+		items.each(function(i, item) {
+			item = jQuery(item);
+			var h = item.outerHeight();
+			var aside = item.parent().children('aside');
+			if(aside.outerHeight() > h) {
+				h = aside.outerHeight();
+			}
+			item.parent().css('min-height', h + "px");
+		});
+	} else {
+		items.each(function(i, item) {
+			jQuery(item).parent().css('min-height', "0px");
+		});
+	}
+}
