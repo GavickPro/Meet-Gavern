360877b40344fe0d6918e5650ceaf2c0992e8ae0
Tomasz Dziuda <dziudek@gmail.com>
2014-10-15 15:53:53 +0200

    Added switcher between universal analytics and the normal one.

--- meet_gavern/lib/framework/helper.social.php
+++ meet_gavern/lib/framework/helper.social.php
@@ -159,7 +159,11 @@ class GKTemplateSocial {
                             $script_code .= '<script type="text/plain" class="cc-onconsent-analytics">';
                         }
                         
-                        $script_code .= 'var _gaq = _gaq || []; _gaq.push([\'_setAccount\', \'' .$key. '\']); _gaq.push([\'_trackPageview\']);(function() { var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s); })();</script>';
+                        if($this->parent->API->get('google_analytics_type','') != 'universal') {
+                        	$script_code .= 'var _gaq = _gaq || []; _gaq.push([\'_setAccount\', \'' .$key. '\']); _gaq.push([\'_trackPageview\']);(function() { var ga = document.createElement(\'script\'); ga.type = \'text/javascript\'; ga.async = true;ga.src = (\'https:\' == document.location.protocol ? \'https://ssl\' : \'http://www\') + \'.google-analytics.com/ga.js\';var s = document.getElementsByTagName(\'script\')[0]; s.parentNode.insertBefore(ga, s); })();</script>';
+                        } else {
+                        	$script_code .= '(function(i,s,o,g,r,a,m){i[\'GoogleAnalyticsObject\']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o), m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m) })(window,document,\'script\',\'//www.google-analytics.com/analytics.js\',\'ga\'); ga(\'create\', \'' . $key . '\', \'auto\'); ga(\'send\', \'pageview\');</script>';
+                        }
                     }
                 }
             }
