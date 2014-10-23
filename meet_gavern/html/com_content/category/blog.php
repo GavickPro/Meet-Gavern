8556335ec25b184265c8272c996e234a8c032b25
Tomasz Dziuda <dziudek@gmail.com>
2014-10-23 14:04:04 +0200

    Fix for the blog view structure.

--- meet_gavern/html/com_content/category/blog.php
+++ meet_gavern/html/com_content/category/blog.php
@@ -69,11 +69,9 @@ JHtml::_('behavior.caption');
 	<?php if (!empty($this->intro_items)) : ?>
 	<?php foreach ($this->intro_items as $key => &$item) : ?>
 	<?php
-		$key= ($key-$leadingcount)+1;
-		$rowcount=( ((int)$key-1) %	(int) $this->columns) +1;
-		$row = $counter / $this->columns;
-
-		if ($rowcount==1) : ?>
+    		$rowcount = ((int) $key % (int) $this->columns) + 1;            
+    		if ($rowcount==1) : 
+    	?>
 	<div class="items-row cols-<?php echo (int) $this->columns;?> <?php echo 'row-'.$row ; ?> row-fluid">
 		<?php endif; ?>
 		<div class="item column-<?php echo $rowcount;?><?php echo $item->state == 0 ? ' system-unpublished' : null; ?> span<?php echo round((12/$this->columns));?>" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
