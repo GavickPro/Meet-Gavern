be9704d426a2fddcc89df94240f5117fe06e7295
sabas <sabas88@gmail.com>
2014-11-17 12:25:08 +0100

    row -> rowcount

--- meet_gavern/html/com_content/category/blog.php
+++ meet_gavern/html/com_content/category/blog.php
@@ -72,7 +72,7 @@ JHtml::_('behavior.caption');
     		$rowcount = ((int) $key % (int) $this->columns) + 1;            
     		if ($rowcount==1) : 
     	?>
-	<div class="items-row cols-<?php echo (int) $this->columns;?> <?php echo 'row-'.$row ; ?> row-fluid">
+	<div class="items-row cols-<?php echo (int) $this->columns;?> <?php echo 'row-'.$rowcount ; ?> row-fluid">
 		<?php endif; ?>
 		<div class="item column-<?php echo $rowcount;?><?php echo $item->state == 0 ? ' system-unpublished' : null; ?> span<?php echo round((12/$this->columns));?>" itemprop="blogPost" itemscope itemtype="http://schema.org/BlogPosting">
 			<?php
